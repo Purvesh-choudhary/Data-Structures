4e1c9a4d0adecb4dd559e2162f4227d06df84d1a
Purveh Choudhary <98455640+Purvesh-choudhary@users.noreply.github.com>
2023-05-28 06:31:37 +0000

    new file:   Data Structures/Final Exam/array 	new file:   Data Structures/Final Exam/array.cpp

---
+++ Data Structures/Final Exam/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void insert(int arr[]){
+    cout<<"Enter value-";
+    cin>>arr[0];
+}
+
+int main(){
+    int arr[10];
+
+    insert(arr);
+
+    return 0;
+}
